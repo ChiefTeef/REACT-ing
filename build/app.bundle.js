9278579c6baa4e19364d2ded2271e9cdeb7b30c0
ChiefTeef <laa6@rice.edu>
2017-06-30 15:30:21 -0700

    added add and checkboxes

--- build/app.bundle.js
+++ build/app.bundle.js
@@ -9772,9 +9772,11 @@ module.exports = __webpack_require__(18);
 "use strict";
 
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
 var _react = __webpack_require__(82);
 
-var react = _interopRequireWildcard(_react);
+var _react2 = _interopRequireDefault(_react);
 
 var _reactDom = __webpack_require__(81);
 
@@ -9782,13 +9784,47 @@ var _reactDom2 = _interopRequireDefault(_reactDom);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-_reactDom2.default.render(React.createElement(
-  'p',
-  null,
-  'React lives!'
-), document.getElementById('root'));
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var dummyData = ["Clean the dog", "eat a bagel", "Take over the world"];
+
+var Todo = function (_React$Component) {
+  _inherits(Todo, _React$Component);
+
+  function Todo(props) {
+    _classCallCheck(this, Todo);
+
+    return _possibleConstructorReturn(this, (Todo.__proto__ || Object.getPrototypeOf(Todo)).call(this, props));
+    // this.state = {
+    //   tasks: dummyData
+  }
+
+  _createClass(Todo, [{
+    key: 'render',
+    value: function render() {
+
+      return _react2.default.createElement(
+        'ul',
+        null,
+        this.props.tasks.map(function (task) {
+          return _react2.default.createElement(
+            'li',
+            { style: { "color": "purple" } },
+            task
+          );
+        })
+      );
+    }
+  }]);
+
+  return Todo;
+}(_react2.default.Component);
+
+_reactDom2.default.render(_react2.default.createElement(TodoList, { tasks: dummyData }), document.getElementById('root'));
 
 /***/ }),
 /* 84 */
--- reactApp/app.js
+++ reactApp/app.js
@@ -1,6 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const dummyData = ["Clean the dog", "eat a bagel", "Take over the world"]
 
-ReactDOM.render(<p>React lives!</p>,
-  document.getElementById('root'));
+
+class TodoList extends React.Component {
+  constructor(props) {
+    super(props);
+    // this.state = {
+    //   tasks: dummyData
+    }
+   render(){
+
+     return (<ul>
+      {this.props.tasks.map((task) => <li key={task}><input type="checkbox" value="1" />{task}</li>)}
+    </ul>)
+   }
+  }
+
+  class InputLine extends React.Component {
+    constructor(props){
+      super(props)
+    }
+    render(){
+      return (
+        <div>
+          <form>
+          <input type="text" name="todoadd"/>
+          <input type="submit" name="submitadd" value="Todo Add" />
+          </form>
+        </div>
+      )
+
+    }
+  }
+
+  class TodoApp extends React.Component {
+    constructor(props){
+      super(props)
+    }
+    render(){
+      return (
+        <div>
+          <InputLine />
+          <TodoList tasks={dummyData} />
+        </div>
+      )
+
+    }
+  }
+
+
+
+ReactDOM.render(<TodoApp />, document.getElementById('root'));
